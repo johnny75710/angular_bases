fdfa268330c82ffbf1bd767a02319bb2dc8c0098
Ionut Felician Federiga <federigaionut@gmail.com>
2023-03-15 15:36:24 +0100

    'comienzo'

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/*Creado el módulo dbz*/
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule { }
